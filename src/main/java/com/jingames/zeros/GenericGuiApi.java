746fb0abf41031e5de5ee4126614fb2b20efa469
Jean-Philippe Krämer <j.kraemer@4allportal.com>
2025-05-30 00:14:11 +0200

    feat: first minor version
    update cnpc+ dependency

--- src/main/java/com/jingames/zeros/GenericGuiApi.java
+++ src/main/java/com/jingames/zeros/GenericGuiApi.java
@@ -21,7 +21,7 @@ import cpw.mods.fml.relauncher.Side;
 public class GenericGuiApi {
 
 	public static final String MODID = "Generic GUI API";
-	public static final String VERSION = "0.0.6";
+	public static final String VERSION = "0.1.0";
 	public static SimpleNetworkWrapper network;
 
 	@Instance(MODID)
