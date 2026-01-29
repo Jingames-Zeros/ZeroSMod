243663e3a6aadf374d0609d4b37de83abba3d227
Trent <coytrenton@gmail.com>
2026-01-29 13:30:03 -0500

    Im not well versed in intellij lol

---
+++ src/main/java/org/darkoro/guiapi/world/ZSBiome3.java
@@ -0,0 +1,25 @@
+package org.darkoro.guiapi.world;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.init.Blocks;
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class ZSBiome3 extends BiomeGenBase {
+
+  public ZSBiome3(int biomeId) {
+    super(biomeId);
+    this.setBiomeName("ZS Biome 3");
+    this.waterColorMultiplier = 0xFFFFF9;
+    this.topBlock = Blocks.grass;
+    this.fillerBlock = Blocks.obsidian;
+    this.spawnableCreatureList.clear();
+    this.spawnableMonsterList.clear();
+  }
+
+  @Override @SideOnly(Side.CLIENT) public int getBiomeGrassColor(int x, int y, int z) { return 0xFFFFF9; }
+  @Override @SideOnly(Side.CLIENT) public int getBiomeFoliageColor(int x, int y, int z) { return 0xFFFFF9; }
+  @Override @SideOnly(Side.CLIENT) public int getSkyColorByTemp(float currentTemperature) { return 0xFFFFF9; }
+  @Override @SideOnly(Side.CLIENT) public int getModdedBiomeGrassColor(int original) { return 0xFFFFF9; }
+  @Override @SideOnly(Side.CLIENT) public int getModdedBiomeFoliageColor(int original) { return 0xFFFFF9; }
+}
