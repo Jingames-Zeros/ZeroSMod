498e39c65248f76dac12bedfda6a2f633f937f9e
Trent <coytrenton@gmail.com>
2026-05-26 13:40:42 -0400

    I SURELY didnt accidentally break previously working stuff

--- src/main/java/org/darkoro/zerosmod/ZeroSMod.java
+++ src/main/java/org/darkoro/zerosmod/ZeroSMod.java
@@ -6,6 +6,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
@@ -86,5 +87,9 @@ public class ZeroSMod {
 
 	@EventHandler public void serverStarting(FMLServerStartingEvent event) {
 		proxy.serverStarting(event);
+	}
+
+	@EventHandler public void postInit(FMLPostInitializationEvent event) {
+		proxy.postInit(event);
 	}
 }
