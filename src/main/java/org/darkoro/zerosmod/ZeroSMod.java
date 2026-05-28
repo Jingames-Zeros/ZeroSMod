36a23ea762289af5a9374764414dab83fc1a974b
Trent <coytrenton@gmail.com>
2026-05-27 20:50:31 -0400

    Versioninginginginginginginginginginginginging

--- src/main/java/org/darkoro/zerosmod/ZeroSMod.java
+++ src/main/java/org/darkoro/zerosmod/ZeroSMod.java
@@ -38,7 +38,7 @@ public class ZeroSMod {
 	public static Logger LOGGER;
 
 	public static final String MODID = "zerosmod";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.2.2";
 	public static SimpleNetworkWrapper network;
 
 	@Instance(MODID)
