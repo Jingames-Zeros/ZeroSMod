962c37d6d9bc0c1ee46864ef189b2a10563a4636
Jean-Philippe K. <jkraemer994@gmail.com>
2026-07-20 22:00:03 +0200

    refactor: biome handling

--- src/main/java/org/darkoro/zerosmod/proxy/ClientProxy.java
+++ src/main/java/org/darkoro/zerosmod/proxy/ClientProxy.java
@@ -3,12 +3,11 @@ package org.darkoro.zerosmod.proxy;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 import java.util.Arrays;
 import java.util.List;
 import org.darkoro.zerosmod.ZeroSMod;
-import org.darkoro.zerosmod.client.JRMCoreRacePatch;
+import org.darkoro.zerosmod.client.BiomeFogHandler;
 import org.darkoro.zerosmod.client.ZSTabOverlayHandler;
 import org.darkoro.zerosmod.input.KeyInputHandler;
 import org.darkoro.zerosmod.input.KeybindHandler;
@@ -17,21 +16,17 @@ import net.minecraftforge.common.MinecraftForge;
 public class ClientProxy extends CommonProxy {
 
   @Override
-  public void preInit(FMLPreInitializationEvent event) {
-    super.preInit(event);
-  }
-
-  @Override
   public void init(FMLInitializationEvent event) {
     super.init(event);
     KeybindHandler.init();
     FMLCommonHandler.instance().bus().register(new KeyInputHandler());
     MinecraftForge.EVENT_BUS.register(new ZSTabOverlayHandler());
+    MinecraftForge.EVENT_BUS.register(new BiomeFogHandler());
   }
 
   @Override
   public void postInit(FMLPostInitializationEvent event) {
-    JRMCoreRacePatch.apply();
+    super.postInit(event);
     String defaultFile = "/assets/zerosmod/lang/%s.lang";
     List<String> langs = Arrays.asList(
         "de_DE", "en_AU", "en_CA", "en_GB", "en_US", "es_AR", "es_ES", "es_MX",
