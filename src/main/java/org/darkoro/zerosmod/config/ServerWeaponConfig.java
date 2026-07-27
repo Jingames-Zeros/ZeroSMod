08444a2a9cce8d53e6740ad4c69b7204d3505db8
noxie <noxie@>
2026-07-27 22:37:47 +0100

    Update range to be a float value, Add initial weapon type configs

--- src/main/java/org/darkoro/zerosmod/config/ServerWeaponConfig.java
+++ src/main/java/org/darkoro/zerosmod/config/ServerWeaponConfig.java
@@ -1,11 +1,17 @@
 package org.darkoro.zerosmod.config;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import org.darkoro.zerosmod.config.defaults.WeaponTypesDefaults;
+import org.darkoro.zerosmod.zsweapons.CachedWeaponState;
+
 import java.io.*;
+import java.util.Map;
 
 public class ServerWeaponConfig {
     private static final String CONFIG_FILE_NAME = "weapon_server.cfg";
     private static boolean ENABLED;
+    private static CachedWeaponState defaultWeaponState;
+    private static Map<String, CachedWeaponState> loadedWeaponStates;
 
     private static final String[] fileHeader = {
             "# ZeroSMod server Weapon System config",
@@ -71,6 +77,7 @@ public class ServerWeaponConfig {
             reader = new BufferedReader(new FileReader(ServerWeaponConfigFile));
             String line;
             String section = "";
+            String currentWeaponType = "";
             while ((line = reader.readLine()) != null) {
                 line = line.trim();
                 if (line.isEmpty() || line.startsWith("#") || line.startsWith(";")) continue;
@@ -89,8 +96,17 @@ public class ServerWeaponConfig {
 
                 String key = line.substring(0, separator).trim();
                 String value = line.substring(separator + 1).trim();
-                if(section.equals("[Combat System]") && key.equals("Disable All Modules")) {
+                if(section.equals(enableConfig[2]) && key.equals("Disable All Modules")) {
                     ENABLED = !value.equals("1");
+                }
+                else if(section.equals(WeaponTypesDefaults.header[0])) {
+                    if(key.equals("type")) {
+                        currentWeaponType = value;
+                        loadedWeaponStates.put(value, new CachedWeaponState());
+                    }
+                    if(!currentWeaponType.equals("")) {
+                        loadWeaponState(loadedWeaponStates.get(currentWeaponType), key, value);
+                    }
                 }
             }
         } catch (IOException ignored) {
@@ -100,6 +116,41 @@ public class ServerWeaponConfig {
                     reader.close();
                 } catch (IOException ignored) {}
             }
+        }
+    }
+
+    public static void loadWeaponState(CachedWeaponState state, String key, String value) {
+        switch(key) {
+            case "Attack Cooldown":
+                state.cooldown = Integer.parseInt(value);
+                break;
+            case "Melee Multiplier":
+                state.attackMultiplier = Float.parseFloat(value);
+                break;
+            case "Melee Range":
+                state.setRange(Float.parseFloat(value));
+                break;
+            case "Can Charge Ki":
+                state.canChargeKi = Boolean.parseBoolean(value);
+                break;
+            case "Ki Additive Damage":
+                // TODO
+                break;
+            case "Ki Multiplier":
+                // TODO
+                break;
+            case "Can Block":
+                state.canBlock = Boolean.parseBoolean(value);
+                break;
+            case "Block Dex Percent":
+                state.blockReduction = Float.parseFloat(value);
+                break;
+            case "Block Cost Multiplier":
+                state.blockCostMultiplier = Float.parseFloat(value);
+                break;
+            case "Block Cooldown":
+                state.blockCooldown = Float.parseFloat(value);
+                break;
         }
     }
 
