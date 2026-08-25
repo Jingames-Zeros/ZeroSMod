7d7a2b06437ed4f1933576fa328af7f5d02d7960
noxie <noxie@>
2026-08-25 11:18:37 +0100

    Reduce sweet spot default damage increase

--- src/main/java/org/darkoro/zerosmod/config/defaults/GeneralWeaponSettings.java
+++ src/main/java/org/darkoro/zerosmod/config/defaults/GeneralWeaponSettings.java
@@ -3,10 +3,10 @@ package org.darkoro.zerosmod.config.defaults;
 public class GeneralWeaponSettings {
     public final static String[] generalSettings = {
         "[General Settings]",
-        "# Sweet spot damage increase in additional multiplier (0.25 = up to 25% damage increase)",
-        "Sweet Spot max damage increase : 10.25",
+        "# Sweet spot damage increase in additional multiplier (0.25 = up to 25% damage increase) (Starts from 0 for some reason)",
+        "Sweet Spot max damage increase : 0.25",
         "# Falloff speed - Blocks for sweet spot damage to completely fall off",
-        "Sweet Spot falloff speed : 2"
+        "Sweet Spot falloff speed : 1.5"
     };
 
     // ADD ALL DEFAULTS TO HERE
